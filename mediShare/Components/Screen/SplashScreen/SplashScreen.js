a2a3fff05023e4b810dac9e0d325392fd30cdca2
prajapatilalit <prajapatilalit150992@gmail.com>
2021-01-31 15:04:00 +0530

    Updated SplashScreen

--- mediShare/Components/Screen/SplashScreen/SplashScreen.js
+++ mediShare/Components/Screen/SplashScreen/SplashScreen.js
@@ -1,13 +1,67 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator, View, StyleSheet, Image, Text } from "react-native";
 
-function SplashScreen(props) {
+// import AsyncStorage from "@react-native-community/async-storage";
+
+const SplashScreen = ({ navigation }) => {
+  //State for ActivityIndicator animation
+  const [animating, setAnimating] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimating(false);
+      //Check if user_id is set or not
+      //If not then send for Authentication
+      //else send to Home Screen
+      // AsyncStorage.getItem("user_id").then((value) =>
+      //   navigation.replace(value === null ? "Auth" : "DrawerNavigationRoutes")
+      // );
+    }, 5000);
+  }, []);
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../../../Images/logo1.jpg")}
+        style={styles.image}
+      />
+      <Text style={styles.text}>medishare</Text>
+      <ActivityIndicator
+        animating={animating}
+        color="#FFFFFF"
+        size="large"
+        style={styles.activityIndicator}
+      />
     </View>
   );
-}
+};
 
 export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#307ecc",
+  },
+  image: {
+    width: 170,
+    height: 170,
+    margin: 30,
+    borderRadius: 100,
+  },
+  text: {
+    fontSize: 30,
+    fontStyle: "italic",
+    textTransform: "uppercase",
+    textShadowColor: "#ffff",
+    color: "#312c51",
+    letterSpacing: 1,
+    fontWeight: "700",
+  },
+  activityIndicator: {
+    alignItems: "center",
+    height: 80,
+  },
+});
